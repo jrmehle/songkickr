828289e5c28b2bc4bfb68a21aea35ccdf759b1d4
Jared Mehle <jrmehle@gmail.com>
2012-02-29 21:17:43 -0600

    Fix billing attribute of performance. Fixes #10

--- lib/songkickr/performance.rb
+++ lib/songkickr/performance.rb
@@ -14,14 +14,14 @@ module Songkickr
   #   }
   class Performance
     attr_accessor :artist, :display_name, :id, :billing_index, :billing
-    
+
     # Takes a the hash of the performance. Parses out an Artist object for the artist.
     def initialize(performance_hash)
       @artist        = Songkickr::Artist.new performance_hash["artist"]
       @display_name  = performance_hash["displayName"]
       @id            = performance_hash["id"]
       @billing_index = performance_hash['billingIndex']
-      @billin        = performance_hash['billing']
+      @billing       = performance_hash['billing']
     end
   end
 end
