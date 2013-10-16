c6387f7e9804d3afbbbe331aa69a0d6a6731b059
Jared Mehle <jared@code42.com>
2013-10-16 12:00:42 -0500

    Added regression test for missing metro_area accessor.

--- test/songkickr/test_venue.rb
+++ test/songkickr/test_venue.rb
@@ -3,14 +3,15 @@ require 'helper'
 class TestVenue < Test::Unit::TestCase
   context "Given an Venue from fixture" do
     setup do
-      @event = Songkickr::Venue.new(fixture_hash('venue_1276231.json')["resultsPage"]["results"]["venue"])
+      @venue = Songkickr::Venue.new(fixture_hash('venue_1276231.json')["resultsPage"]["results"]["venue"])
     end
 
     should "properly load data from JSON" do
-      assert_equal "Branch House Amphitheater", @event.display_name
-      assert_equal 1276231, @event.id
-      assert_equal 38.558191, @event.lat
-      assert_equal -122.584747, @event.lng
+      assert_equal "Branch House Amphitheater", @venue.display_name
+      assert_equal 1276231, @venue.id
+      assert_equal 38.558191, @venue.lat
+      assert_equal -122.584747, @venue.lng
+      assert_not_nil @venue.metro_area
     end
   end
 end
